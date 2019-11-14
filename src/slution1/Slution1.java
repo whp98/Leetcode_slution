345209297783372275b8c72853441d8b774f1804
whp <whp98@outlook.com>
2019-11-14 23:32:38 +0800

    穷举法也分优劣

--- src/slution1/Slution1.java
+++ src/slution1/Slution1.java
@@ -1,10 +1,24 @@
 package slution1;
-//暴力算法，穷举法
 class Solution1 {
+    //穷举法一
+    // 通过	36 ms	37.3 MB
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[j]+nums[i]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }
+    //穷举法二
+    //通过	17 ms	37.5 MB
+    public int[] twoSum1(int[] nums, int target) {
+        for(int i=0;i<nums.length-1;i++){
+            int a = target-nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]==a){
                     return new int[]{i,j};
                 }
             }
